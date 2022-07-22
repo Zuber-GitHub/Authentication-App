a423e15c76aedc812899654a73863eda789c2920
Zuber-GitHub <zuberriyazahmed@gmail.com>
2022-07-22 12:11:23 +0530

    Preserving Login Status Added

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import CartContext from '../Context/cart-contetxt';
-import CartProvider from '../Context/CartProvider';
+
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = (props) => {
   const credCtx = useContext(CartContext);
+  const isLoggedIn = credCtx.isLoggedIn;
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -14,15 +15,22 @@ const MainNavigation = (props) => {
       </Link>
       <nav>
         <ul>
-          <li>
-          {!credCtx.check() && <Link to='/auth'>Login</Link>}
-          </li>
-          {credCtx.check() && <li>
+        {!isLoggedIn && 
+        (<li>
+            <Link to='/auth'>Login</Link>
+          </li>)
+          }
+
+          {isLoggedIn && 
+          (<li>
             <Link to='/profile'>Profile</Link>
-          </li>}
-          <li>
-            {credCtx.check() && <button onClick={credCtx.logOut}>Logout</button>}
-          </li>
+          </li>)
+          }
+          {isLoggedIn && 
+          (<li>
+            <button onClick={credCtx.logOut}>Logout</button>
+          </li>)
+          }
         </ul>
       </nav>
     </header>
